6ea1dde83d846e63f401a8898ac64be877a2dcea
AdityaViroopaxi <aviroopaxi@gmail.com>
2026-06-24 10:37:39 +0530

    Added Db variables

--- backend/modules/db.py
+++ backend/modules/db.py
@@ -10,14 +10,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    "host":     os.getenv("MYSQL_HOST",     "localhost"),
-    "port":     int(os.getenv("MYSQL_PORT", 3306)),
-    "user":     os.getenv("MYSQL_USER",     "root"),
-    "password": os.getenv("MYSQL_PASSWORD", "root"),
-    "database": os.getenv("MYSQL_DATABASE", "ntdap_db"),
+    "host":     os.getenv("MYSQLHOST"),
+    "port":     int(os.getenv("MYSQLPORT", 3306)),
+    "user":     os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
 }
-
-
 def get_connection():
     return mysql.connector.connect(**DB_CONFIG)
 
